a2d65ca3a8c992336c2a877acd4c80f49c0c6c39
lschaertel <ludwigschaertel@gmail.com>
2020-05-15 18:51:21 +0200

    finished exercise 8

--- code_samples/exersices/exercise_8/main.go
+++ code_samples/exersices/exercise_8/main.go
@@ -1,25 +1,189 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"sort"
+)
 
+type user struct {
+	First   string
+	Last    string
+	Age     int
+	Sayings []string
+}
+
+func main() {
+	exercise1()
+	exercise2()
+	exercise3()
+	exercise4()
+	exercise5()
 }
 
 func exercise1() {
+	u1 := user{
+		First: "James",
+		Age:   32,
+	}
 
+	u2 := user{
+		First: "Moneypenny",
+		Age:   27,
+	}
+
+	u3 := user{
+		First: "M",
+		Age:   54,
+	}
+
+	users := []user{u1, u2, u3}
+
+	fmt.Println(users)
+
+	b, err := json.Marshal(users)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(b))
 }
+
 func exercise2() {
+	s := `[{"First":"James","Last":"Bond","Age":32,"Sayings":["Shaken, not stirred","Youth is no guarantee of innovation","In his majesty's royal service"]},{"First":"Miss","Last":"Moneypenny","Age":27,"Sayings":["James, it is soo good to see you","Would you like me to take care of that for you, James?","I would really prefer to be a secret agent myself."]},{"First":"M","Last":"Hmmmm","Age":54,"Sayings":["Oh, James. You didn't.","Dear God, what has James done now?","Can someone please tell me where James Bond is?"]}]`
+	fmt.Println(s)
+	var users []user
+	err := json.Unmarshal([]byte(s), &users)
+	if err != nil {
+		println(err)
+	}
+	fmt.Println(users)
 
 }
 func exercise3() {
+	u1 := user{
+		First: "James",
+		Last:  "Bond",
+		Age:   32,
+		Sayings: []string{
+			"Shaken, not stirred",
+			"Youth is no guarantee of innovation",
+			"In his majesty's royal service",
+		},
+	}
 
+	u2 := user{
+		First: "Miss",
+		Last:  "Moneypenny",
+		Age:   27,
+		Sayings: []string{
+			"James, it is soo good to see you",
+			"Would you like me to take care of that for you, James?",
+			"I would really prefer to be a secret agent myself.",
+		},
+	}
+
+	u3 := user{
+		First: "M",
+		Last:  "Hmmmm",
+		Age:   54,
+		Sayings: []string{
+			"Oh, James. You didn't.",
+			"Dear God, what has James done now?",
+			"Can someone please tell me where James Bond is?",
+		},
+	}
+
+	users := []user{u1, u2, u3}
+
+	fmt.Println(users)
+
+	err := json.NewEncoder(os.Stdout).Encode(users)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
+
 func exercise4() {
+	xi := []int{5, 8, 2, 43, 17, 987, 14, 12, 21, 1, 4, 2, 3, 93, 13}
+	xs := []string{"random", "rainbow", "delights", "in", "torpedo", "summers", "under", "gallantry", "fragmented", "moons", "across", "magenta"}
 
+	fmt.Println(xi)
+	sort.Ints(xi)
+	fmt.Println(xi)
+
+	fmt.Println(xs)
+	sort.Strings(xs)
+	fmt.Println(xs)
 }
+
+type ByAge []user
+
+func (a ByAge) Len() int           { return len(a) }
+func (a ByAge) Less(i, j int) bool { return a[i].Age < a[j].Age }
+func (a ByAge) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+
+type ByName []user
+
+func (a ByName) Len() int           { return len(a) }
+func (a ByName) Less(i, j int) bool { return []byte(a[i].Last)[0] < []byte(a[j].Last)[0] }
+func (a ByName) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+
 func exercise5() {
+	u1 := user{
+		First: "James",
+		Last:  "Bond",
+		Age:   32,
+		Sayings: []string{
+			"Shaken, not stirred",
+			"Youth is no guarantee of innovation",
+			"In his majesty's royal service",
+		},
+	}
 
-}
+	u2 := user{
+		First: "Miss",
+		Last:  "Moneypenny",
+		Age:   27,
+		Sayings: []string{
+			"James, it is soo good to see you",
+			"Would you like me to take care of that for you, James?",
+			"I would really prefer to be a secret agent myself.",
+		},
+	}
 
-func exercise6() {
+	u3 := user{
+		First: "M",
+		Last:  "Hmmmm",
+		Age:   54,
+		Sayings: []string{
+			"Oh, James. You didn't.",
+			"Dear God, what has James done now?",
+			"Can someone please tell me where James Bond is?",
+		},
+	}
 
+	users := []user{u1, u2, u3}
+
+	fmt.Println(users)
+
+	sort.Sort(ByAge(users))
+	fmt.Println("--- Users sorted by Age ---")
+	for _, v := range users {
+		sort.Strings(v.Sayings)
+		fmt.Printf("%v, %v  %v\n\t%v\n", v.First, v.Last, v.Age, v.Sayings)
+	}
+
+	sort.Sort(ByName(users))
+	fmt.Println("--- Users sorted by Name ---")
+	for _, v := range users {
+		sort.Strings(v.Sayings)
+		fmt.Printf("%v, %v  %v\n\t%v\n", v.First, v.Last, v.Age, v.Sayings)
+	}
+
+	fmt.Println("--- Sayings of Users sorted ---")
+	for _, v := range users {
+		sort.Strings(v.Sayings)
+		fmt.Printf("%v, %v  %v\n\t%v\n", v.First, v.Last, v.Age, v.Sayings)
+	}
 }
